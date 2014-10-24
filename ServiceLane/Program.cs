c1ddbaa68f831c19f318682d7a55915991e7c15e
Tanmay Dharmaraj <get2tanmay@gmail.com>
2014-10-24 15:25:42 +0530

    Initial Commit

---
+++ ServiceLane/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                string[] inputVals = input.Split(' ');
+                if (inputVals.Length != 2)
+                {
+                    Console.WriteLine("Invalid input. Input 2 numbers separated with a single space");
+                    return;
+                }
+                int freewayLength = Convert.ToInt32(inputVals[0]);
+                int testCaseLength = Convert.ToInt32(inputVals[1]);
+                string[] output = new string[testCaseLength];
+                string freewayValues = Console.ReadLine();
+
+                string[] str_freeway = freewayValues.Split(' ');
+                if (str_freeway.Length != freewayLength)
+                {
+                    Console.WriteLine("Invalid input. Input " + freewayLength + " numbers separated with a single space");
+                    return;
+                }
+                //build the freeway
+                int[] freeway = new int[freewayLength];
+                for (int i = 0; i < freewayLength; i++)
+                {
+                    freeway[i] = Convert.ToInt32(str_freeway[i]);
+                }
+
+
+                for (int i = 0; i < testCaseLength; i++)
+                {
+                    string testCase = Console.ReadLine();
+                    int segmentEnter = Convert.ToInt32(testCase.Split(' ')[0]);
+                    int segmentExit = Convert.ToInt32(testCase.Split(' ')[1]);
+                    int result = freeway[segmentEnter];
+                    for (int j = segmentEnter; j <= segmentExit; j++)
+                    {
+                        if (result > freeway[j])
+                            result = freeway[j];
+                    }
+                    output[i] = result.ToString();
+                }
+
+                //print the output
+                foreach (string outValue in output)
+                {
+                    Console.WriteLine(outValue);
+                }
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error ! " + Environment.NewLine + e.Message);
+            }
+        }
+    }
+}
---
+++ UtopianSapling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopianSapling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int height = 0;
+            int TestCase_input = Convert.ToInt32(Console.ReadLine());
+            int[] TestCases = new int[TestCase_input];
+            for (int testCount = 0; testCount < TestCases.Length; testCount++)
+            {
+                TestCases[testCount] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int testCase = 0; testCase < TestCases.Length; testCase++)
+            {
+                height = 1;
+                int N = TestCases[testCase];
+                for (int i = 0; i <= N; i++)
+                {
+                    if (i == 0)
+                    {
+                        //height++;
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        //double
+                        height++;
+                    }
+                    else
+                    {
+                        height = height * 2;
+                    }
+                }
+                Console.WriteLine(height);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
