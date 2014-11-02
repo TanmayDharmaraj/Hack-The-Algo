777aeb798023d5d3038c8089073d98b103357825
Tanmay Dharmaraj <get2tanmay@gmail.com>
2014-11-02 15:50:10 +0530

    Gem Stones Completed

--- Gem Stones/Program.cs
+++ Gem Stones/Program.cs
@@ -8,7 +8,46 @@ namespace Gem_Stones
         static void Main(string[] args)
         {
             int N = Convert.ToInt32(Console.ReadLine());
-            string[] rocks = new string[N];
+            string input;
+            int[,] chr_array = new int[N, 26];
+            int answer = 0;
+            for (int i = 0; i < N; i++)
+            {
+                input = Console.ReadLine();
+                char[] tempArray = input.ToCharArray();
+                foreach (char c in tempArray)
+                    chr_array[i, (int)c - 'a']++;
+
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                int flag = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    if (chr_array[j, i] == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    answer++;
+                }
+            }
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+
+            /* Below is the old code. It basically does the same thing but in a little complicated way which made
+             * the test case to fail.
+             */
+            #region Old code
+            /*
+            int N = Convert.ToInt32(Console.ReadLine());
+            char[] rocks = new char[N];
+            
             for (int i = 0; i < N; i++)
             {
                 rocks[i] = Console.ReadLine();
@@ -25,19 +64,23 @@ namespace Gem_Stones
                 }
             }
             char[] c = rocks[index].ToCharArray();
+            Console.WriteLine("Character = "+c[1]);
             int count = 0;
             for (int i = 0; i < c.Length; i++)
             {
                 int temp = 0;
-                for (int j = 0; j < rocks.Length; j++)
-                    if (rocks[j].Contains(c[i]))
+                foreach (string rock in rocks)
+                {
+                    if (rock.Contains(c[i]))
                         temp++;
-
+                }
                 if (rocks.Length == temp)
                     count++;
             }
             Console.WriteLine(count);
-            Console.ReadLine();
+            Console.ReadLine();*/
+            #endregion
+        
         }
     }
 }
